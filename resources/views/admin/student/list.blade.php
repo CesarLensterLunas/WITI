7f7a5ecc26d34af9dc49844c266b299e81701ed6
abc <abc@gmail.com>
2024-04-01 12:49:32 +0800

    New project to push to GitHub

--- resources/views/admin/student/list.blade.php
+++ resources/views/admin/student/list.blade.php
@@ -36,15 +36,27 @@
                 <h3 class="card-title">Student List</h3>
               </div>
               <!-- /.card-header -->
-              <div class="card-body p-0">
+              <div class="card-body p-0" style="overflow: auto;">>
                 <table class="table table-striped">
                   <thead>
-                    <tr>
-                      <th >#</th>
-                      <th>Name</th>
-                      <th>Email</th>
-                      <th >Created Date</th>
-                      <th >Action</th>
+                  <tr>
+                  <th>#</th>
+                  <th>Profile Pic</th> 
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Admission Number</th>
+                  <th>Roll Number</th>
+                  <th>Class</th>
+                 <th>Gender</th>
+                 <th>Date of Birth </th>
+                  <th>Mobile Number</th> <th>Admission Date</th> <th>Blood Group</th>
+                  <th>Height</th> 
+                  <th>Weight</th>
+                   <th>Status</th>
+                  <th>Created Date</th>
+                  <th>Action</th>
+      
+
                     </tr>
                   </thead>
                   <tbody>
@@ -52,12 +64,39 @@
                     @foreach ($getRecord as $value)
                     <tr>
                       <td>{{ $value->id }}</td>
+                      <td>
+                        
+@if(!empty($value->getProfile()))
+<img src="{{ $value->getProfile() }}" style="height: 50px; width:50px; border-radius: 50px;"> 
+@endif
+
+
                       <td>{{ $value->name }}</td>
                       <td>{{ $value->email }}</td>
+                      <td>{{ $value->	admission_number	 }}</td>
+                      <td>{{ $value->roll_number }}</td>
+                      <td>{{ $value->class_name }}</td>
+                      <td>{{ $value->gender }}</td>
+
+                      <td>
+                       @if(!empty($value->date_of_birth))
+                       {{ date('d-m-Y', strtotime($value->date_of_birth)) }} 
+                       @endif
+                      </td>
+                      <td>{{ $value->mobile_number }}</td>
+                      <td>
+                      @if (!empty($value->admission_date))
+                       {{ date('d-m-Y', strtotime($value->admission_date)) }} 
+                       @endif
+                      </td>
+                      <td>{{ $value->blood_group }}</td>
+                      <td>{{ $value->height }}</td>
+                      <td>{{ $value->weight }}</td>
+                       <td>{{ ($value->status == 0 ) ? 'Active': 'Inactive' }}</td>
                       <td>{{ date ('d-m-Y H:i A',strtotime($value->created_at)) }}</td>
-                      <td> 
-                      <a href="{{ url('admin/student/edit/'.$value->id) }}" class="btn btn-primary">Edit</a>
-                      <a href="{{ url('admin/student/Delete/'.$value->id) }}" class="btn btn-danger">Delete</a>
+                      <td style="min-width: 150px;">
+                      <a href="{{ url('admin/student/edit/'.$value->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                      <a href="{{ url('admin/student/delete/'.$value->id) }}" class="btn btn-danger btn-sm">Delete</a>
                        </td>
 
                       </tr>
--- routes/web.php
+++ routes/web.php
@@ -45,11 +45,16 @@ Route::group(['middleware'=>'admin'], function(){
     Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);    
     Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
     Route::get('admin/admin/Delete/{id}', [AdminController::class, 'Delete']);
+    
     //student
     
     Route::get('admin/student/list', [StudentController::class, 'list']); 
     Route::get('admin/student/add', [StudentController::class, 'add']); 
     Route::post('admin/student/add', [StudentController::class, 'insert']);
+    Route::get('admin/student/edit/{id}', [StudentController::class, 'edit']); 
+    Route::post('admin/student/edit/{id}', [StudentController::class, 'update']);
+    Route::get('admin/student/delete/{id}', [StudentController::class, 'delete']);
+
 
     //class url
     Route::get('admin/class/list', [ClassController::class, 'list']);
